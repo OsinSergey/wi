b8829665d5f7b3dc98462897f14cdaf60de9de33
OsinSergey <osin_osin@mail.ru>
2020-03-24 03:17:58 +0600

    First Commit

---
+++ app/js/_custom.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+	let parent = document.querySelector('.container');
+
+	addEventListener('click' , ()=>{
+		
+		if (parent.style.display === 'none') {
+			parent.style.display = 'block';
+		} else {
+			parent.style.display = 'none';
+		}
+	});
+
+
+	
+
+
+
+	// Custom JS
+
+});
