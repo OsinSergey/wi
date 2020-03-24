eca7c90dba4e261e17157cc4ce3bf612599696cf
OsinSergey <osin_osin@mail.ru>
2020-03-24 13:10:09 +0600

    Начала сайта

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,14 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-	let parent = document.querySelector('.container');
-
-	addEventListener('click' , ()=>{
-		
-		if (parent.style.display === 'none') {
-			parent.style.display = 'block';
-		} else {
-			parent.style.display = 'none';
-		}
-	});
 
 
 	
