d69cdcd2100c2167bdbed665908442075339f735
OsinSergey <osin_osin@mail.ru>
2020-03-27 14:55:42 +0600

    Табы

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	
+let tabsItem = $('.tabs_link');	
+
+tabsItem.on('click', function(e){
+	e.preventDefault();
+	$('.tabs-active').toggleClass('tabs-active');
+	$(this).toggleClass('tabs-active');
+	let activContent = $(this).attr('href');
+	$('.visible').toggleClass('visible');
+	$(activContent).toggleClass('visible');
+
+
+});
 
 	
 
